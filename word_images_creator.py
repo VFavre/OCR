78008554d0753363f584dcbe77d3ac6358bf7b91
Victor Favre <[victor.favre@protonmail.com](mailto:victor.favre@protonmail.com)>
2020-03-26 00:18:05 +0100

    ajout du bruit aléatoire

--- word_images_creator.py
+++ word_images_creator.py
@@ -1,4 +1,7 @@
+import random
 from PIL import Image, ImageDraw, ImageFont
+
+#create text tilted at 7 degre
 
 label = 'Hellq!' 
 font = ImageFont.truetype('C:\Windows\Fonts\Calibri.ttf', 36)
@@ -7,6 +10,24 @@ fontimage = Image.new('L', (font.getsize(label)[0], line_height))
 ImageDraw.Draw(fontimage).text((0, 0), label, fill=255, font=font)
 fontimage = fontimage.rotate(7, resample=Image.BICUBIC, expand=True)
 
-orig = Image.new('L',(fontimage.size[0],fontimage.size[1]), color = 'white')
-orig.paste("black", box=(0, 0), mask=fontimage)
-orig.save("test.png")
+final = Image.new('L',(fontimage.size[0],fontimage.size[1]), color = 'white')
+final.paste("black", box=(0, 0), mask=fontimage)
+
+
+# add noise to the text
+length = fontimage.size[1]
+height = fontimage.size[0]
+
+number_of_pixels = length * height
+pixels = final.load()
+
+noise_factor = int(0.3*(number_of_pixels))
+
+for i in range(noise_factor):
+    x= int(random.uniform(0, height))
+    y= int(random.uniform(0, length))
+    random.seed(int(random.uniform(0, number_of_pixels)))
+    pixels[x,y] = 0
+
+
+final.save("test.png")
